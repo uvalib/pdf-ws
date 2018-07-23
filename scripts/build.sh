e2390a40fa89bc83007d060a08f84604e4fac7eb
Jason Jordan <jlj5aj@virginia.edu>
2018-07-23 13:17:11 -0400

    support standardized project layout

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-env GOOS=linux go build -o dist/pdf-ws.linux
-cp config.yml.template dist/config.yml
-cp index.html dist/index.html
+
+# get git project directory relative to this script's location,
+# so that this script can be run from anywhere
+basedir="$(dirname "$0")/.."
+srcdir="${basedir}/cmd/pdf-ws"
+distdir="${basedir}/dist"
+cfgdir="${basedir}/configs"
+webdir="${basedir}/web"
+
+# clear out any existing files
+[ -d "$distdir" ] && rm -rf "$distdir"
+
+# change to source directory and build app
+cd "$srcdir" || exit 1
+
+env GOOS=linux go build -o "${distdir}/pdf-ws.linux"
+cp -f "${cfgdir}/config.yml.template" "${distdir}/config.yml"
+cp -f "${webdir}/index.html" "${distdir}/index.html"
