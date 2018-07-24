2d472c163db8fcd69bf030e54a5017707a924441
Jason Jordan <jlj5aj@virginia.edu>
2018-07-24 17:41:55 -0400

    rework build process for teamcity/docker integration

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,22 +16,33 @@ cfgdir="${gitdir}/configs"
 webdir="${gitdir}/web"
 distdir="${gitdir}/dist"
 
+echo "git directory: [$gitdir]"
 # sanity checks
 [ ! -d "$srcdir" ] && die "missing source directory: [$srcdir]"
 [ ! -d "$cfgdir" ] && die "missing configuration directory: [$cfgdir]"
 [ ! -d "$webdir" ] && die "missing web directory: [$webdir]"
 
 # clear out any existing dist files
+echo -n "removing old distribution files... "
 [ -d "$distdir" ] && rm -rf "$distdir"
-
-# change to source directory and build app
-cd "$srcdir" || exit 1
-
-echo -n "building app... "
-env GOOS=linux go build -o "${distdir}/pdf-ws.linux" || die "build failed"
 echo "OK"
 
-echo -n "copying assets..."
+# ugly prep work
+echo -n "setting up go environment... "
+rm -f src cmd/pdf-ws/vendor
+ln -sf cmd src
+ln -sf ../../vendor cmd/pdf-ws/vendor
+echo "OK"
+
+# change to source directory and build app
+#cd "$srcdir" || exit 1
+
+echo -n "building app... "
+#env GOOS=linux go build -o "${distdir}/pdf-ws.linux" || die "build failed"
+GOPATH="$gitdir" CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o "${distdir}/pdf-ws.linux" cmd/pdf-ws/*.go || die "build failed"
+echo "OK"
+
+echo -n "copying assets... "
 cp -f "${cfgdir}/config.yml.template" "${distdir}/config.yml"
 cp -f "${webdir}/index.html" "${distdir}/index.html"
 echo "OK"
