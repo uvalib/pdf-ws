f687100779a6a88a3d758ffde0888c874470bd48
Jason Jordan <jlj5aj@virginia.edu>
2018-08-01 13:07:26 -0400

    add filesystem check

--- cmd/pdf-ws/healthcheck.go
+++ cmd/pdf-ws/healthcheck.go
@@ -3,40 +3,49 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
 	"time"
+	"os"
+	"math/rand"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func healthCheckHandler(rw http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	rw.Header().Set("Content-Type", "application/json")
-
+func checkTracksysDatabase() bool {
 	// check tracksys database connection
-	log.Printf("Checking Tracksys...")
 
 	tsPid := "tsb:18139"
 	cnt, expected_cnt := 0, 1
 	tsStatus := true
 
+	logger.Printf("[HEALTH] [Tracksys] checking for PID: [%s]", tsPid)
+
 	qs := "select count(*) as cnt from metadata b where pid=?"
 	db.QueryRow(qs, tsPid).Scan(&cnt)
 
+	logger.Printf("[HEALTH] [Tracksys] record count: expected %d, got %d", expected_cnt, cnt)
+
 	if cnt != expected_cnt {
-		log.Printf("ERROR: Tracksys database: count mismatch: expected %d, got %d", expected_cnt, cnt)
+		logger.Printf("[HEALTH] [Tracksys] ERROR: count mismatch")
 		tsStatus = false
 	}
 
+	logger.Printf("[HEALTH] [Tracksys] SUCCESS")
+
+	return tsStatus
+}
+
+func checkIIIFService() bool {
 	// check IIIF server
-	log.Printf("Checking IIIF...")
 
 	iiifPid := "tsm:1250722"
 	size, expected_size := 0, 154948
 	iiifStatus := true
 
-	url := config.iiifUrlTemplate
+	logger.Printf("[HEALTH] [IIIF] checking for PID: [%s]", iiifPid)
+
+	url := config.iiifUrlTemplate.value
 	url = strings.Replace(url, "$PID", iiifPid, 1)
 
 	timeout := time.Duration(5 * time.Second)
@@ -45,24 +54,75 @@ func healthCheckHandler(rw http.ResponseWriter, req *http.Request, params httpro
 	}
 	resp, err := client.Get(url)
 	if err != nil {
-		log.Printf("ERROR: IIIF service: Get: (%s)", err)
+		logger.Printf("[HEALTH] [IIIF] ERROR: Get: (%s)", err)
 		iiifStatus = false
 	} else {
 		b, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			log.Printf("ERROR: IIIF service: ReadAll: (%s)", err)
+			logger.Printf("[HEALTH] [IIIF] ERROR: ReadAll: (%s)", err)
 			iiifStatus = false
 		} else {
 			resp.Body.Close()
 			size = len(b)
+
+			logger.Printf("[HEALTH] [IIIF] image size: expected %d, got %d", expected_size, size)
+
 			if size != expected_size {
-				log.Printf("ERROR: IIIF service: size mismatch: expected %d, got %d", expected_size, size)
+				logger.Printf("[HEALTH] [IIIF] size mismatch")
 				iiifStatus = false
 			}
 		}
 	}
 
-	out := fmt.Sprintf(`{"alive": true, "iiif": %t, "tracksys": %t}`, iiifStatus, tsStatus)
+	logger.Printf("[HEALTH] [IIIF] SUCCESS")
+
+	return iiifStatus
+}
+
+func checkFilesystem() bool {
+	// create a test file in a random directory to ensure filesystem exists and is writeable
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	testPath := fmt.Sprintf("%s/%05d-%x", config.storageDir.value, os.Getpid(), r.Int63())
+
+	logger.Printf("[HEALTH] [Filesystem] creating random test directory: [%s]", testPath)
+
+	if mkdirErr := os.Mkdir(testPath, 0777); mkdirErr != nil {
+		logger.Printf("[HEALTH] [Filesystem] ERROR: Mkdir: (%s)", mkdirErr)
+		return false
+	}
+	defer os.RemoveAll(testPath)
+
+	logger.Printf("[HEALTH] [Filesystem] creating test file")
+
+	testFile, openErr := os.OpenFile(fmt.Sprintf("%s/test.txt", testPath), os.O_CREATE|os.O_RDWR, 0666)
+	if openErr != nil {
+		logger.Printf("[HEALTH] [Filesystem] ERROR: OpenFile: (%s)", openErr)
+		return false
+	}
+	defer testFile.Close()
+
+	logger.Printf("[HEALTH] [Filesystem] writing to test file")
+
+	if _, writeErr := testFile.WriteString("test"); writeErr != nil {
+		logger.Printf("[HEALTH] [Filesystem] ERROR: WriteString: (%s)", writeErr)
+		return false
+	}
+
+	logger.Printf("[HEALTH] [Filesystem] SUCCESS")
+
+	return true
+}
+
+func healthCheckHandler(rw http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	rw.Header().Set("Content-Type", "application/json")
+
+	tsStatus := checkTracksysDatabase()
+	iiifStatus := checkIIIFService()
+	fsStatus := checkFilesystem()
+
+	out := fmt.Sprintf(`{"alive": true, "iiif": %t, "tracksys": %t, "storage": %t}`, iiifStatus, tsStatus, fsStatus)
 
 	if iiifStatus == false {
 		http.Error(rw, out, http.StatusInternalServerError)
