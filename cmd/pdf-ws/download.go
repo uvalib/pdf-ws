c50f734f6bcef9d003f5840711ca16e6e4544e67
Jason Jordan <jlj5aj@virginia.edu>
2018-10-02 14:19:05 -0400

    support seamless transition from old locations; include file size in http headers

--- cmd/pdf-ws/download.go
+++ cmd/pdf-ws/download.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -23,6 +24,8 @@ func downloadHandler(w http.ResponseWriter, r *http.Request, params httprouter.P
 		fmt.Fprintf(w, "Not found")
 		return
 	}
+
+	/* get path of file to send from the done file */
 	f, err := os.OpenFile(fmt.Sprintf("%s/done.txt", workDir), os.O_RDONLY, 0666)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
@@ -35,14 +38,42 @@ func downloadHandler(w http.ResponseWriter, r *http.Request, params httprouter.P
 		fmt.Fprintf(w, "Unable to find PDF for this PID")
 		return
 	}
-	logger.Printf("Sending %s to client", b)
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.pdf", pid))
-	w.Header().Set("Content-Type", "application/pdf")
-	in, err := os.Open(string(b))
+
+	pdfFile := strings.TrimSpace(string(b))
+
+	/* seamless conversion from old locations */
+	if strings.HasPrefix(pdfFile,"tmp/") {
+		logger.Printf("Old location: [%s]",pdfFile)
+		pdfFile = strings.Replace(pdfFile,"tmp",config.storageDir.value,1)
+		logger.Printf("New location: [%s]",pdfFile)
+
+		/* write out new location */
+	}
+
+	/* get file size */
+
+	in, err := os.Open(pdfFile)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Unable to open PDF for this PID")
 		return
 	}
 	defer in.Close()
+
+	stat, staterr := in.Stat()
+	if staterr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Unable to stat PDF for this PID")
+		return
+	}
+
+	logger.Printf("Sending %s to client with size %d", pdfFile, stat.Size())
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.pdf", pid))
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Header().Set("Content-Length", fmt.Sprint(stat.Size()));
+
 	io.Copy(w, in)
-	logger.Printf("PDF for %s completed successfully", pid)
+
+	logger.Printf("PDF download for %s completed successfully", pid)
 }
