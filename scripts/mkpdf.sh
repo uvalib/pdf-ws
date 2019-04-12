c9d27815098994d6a424d55cbfe0f5e1018bfc79
Jason Jordan <jlj5aj@virginia.edu>
2019-04-12 17:58:33 -0400

    include cover in generated pdf

--- scripts/mkpdf.sh
+++ scripts/mkpdf.sh
@@ -191,8 +191,7 @@ cd "$workdir" || die "could not change to directory: [$workdir]"
 
 create_cover_image
 
-#create_partial_pdfs "cover.png" "$@"
-create_partial_pdfs "$@"
+create_partial_pdfs "cover.png" "$@"
 
 merge_partial_pdfs
 
